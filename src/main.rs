b263b01c1a99d769dff5975257e5edb2b8e98cc4
JethroNatividad <natividad.jet@gmail.com>
2024-02-04 06:57:20 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 // Write a program that generates password with special characters, and numbers
+// Inputs: length, n_special_characters, n_numbers
+// Process: generate random password
+// Output: random password
 fn main() {
     println!("Hello, world!");
 }
