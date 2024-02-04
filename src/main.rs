3696158decba24c64e7cd892b0304004fd5373a2
JethroNatividad <natividad.jet@gmail.com>
2024-02-04 14:49:03 +0800

    implement get input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use rand::Rng;
+use std::io;
+use std::io::Write;
 
 // Write a program that generates password with special characters, and numbers
 // Inputs: length, has_lowercase, has_uppercase, has_special_characters, has_numbers
@@ -151,6 +153,24 @@ mod tests {
         assert_eq!(password.len(), 0, "Password length should be 0");
     }
 }
+
+fn get_input<T: std::str::FromStr>(prompt: &str) -> T {
+    loop {
+        print!("{}", prompt);
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+
+        match input.trim().parse() {
+            Ok(value) => break value,
+            Err(_) => println!("Invalid input. Please try again."),
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
